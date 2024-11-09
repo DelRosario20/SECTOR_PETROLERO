452d763e347d9739fe6383f277d92681bdc07f4f
Alan Del Rosario <162060860+DelRosario20@users.noreply.github.com>
2024-11-09 13:50:47 -0500

    Actualización de la información del sector financiero

--- code/code_v15-09-24/code_v11_10_24.R
+++ code/code_v15-09-24/code_v11_10_24.R
@@ -1,20 +1,23 @@
 # Limpieza de datos -------------------------------------------------------
+{
 library(readxl)
 library(tidyverse)
 library(lubridate)
 library(psych)
-
+} # libreria
+?describe()
 
 setwd("C:/Users/USER/Documents/YRF_PROJECT/")
 
 data_inicial = read_excel("data/name_variable/SerieCifrasPetroleras_1_v.xlsx", skip=1)
 
+{
 data_1 = data_inicial %>%
   filter(`...1` == "TOTAL NACIONAL-Producción de Petróleo Crudo" | 
            #`...1` == "- Producción Promedio Diaria-Petróleo Crudo Nacional" | 
            #`...1` == "- Tasa de crecimiento anual (t / t-12) (1)-Petróleo Crudo Nacional" | 
            #`...1` == "- Tasa de crecimiento mensual (t / t-1) (1)-Petróleo Crudo Nacional" | 
-           `...1` == "EMPRESAS PÚBLICAS-Producción de Petróleo Crudo" | 
+           #`...1` == "EMPRESAS PÚBLICAS-Producción de Petróleo Crudo" | 
            #`...1` == "EP Petroecuador (2)" | 
            #`...1` == "Petroamazonas EP(3)(4)(5)(6)" | 
            #`...1` == "Operadora Río Napo (Sacha)(4)" | 
@@ -23,11 +26,11 @@ data_1 = data_inicial %>%
            #`...1` == "- Petroamazonas EP Producción Promedio Diario" | 
            #`...1` == "- Operad. Río Napo Producción Promedio Diario" | 
            #`...1` == "- Crudo recuperado Amazonía Viva (7)" | 
-           `...1` == "COMPAÑÍAS PRIVADAS*-Producción de Petróleo Crudo" | 
+           #`...1` == "COMPAÑÍAS PRIVADAS*-Producción de Petróleo Crudo" | 
            #`...1` == "- Producción Promedio Diaria-Compañías Privadas" | 
            `...1` == "TOTAL NACIONAL-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
            #`...1` == "- Producción Promedio Diaria-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
-           `...1` == "EMPRESAS PÚBLICAS-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
+           #`...1` == "EMPRESAS PÚBLICAS-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
            #`...1` == "- Producción Promedio Diaria-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
            #`...1` == "Petroproducción" | 
            #`...1` == "EP Petroecuador (8)" | 
@@ -38,14 +41,14 @@ data_1 = data_inicial %>%
            #`...1` == "Petroamazonas EP Bloque 60" | 
            #`...1` == "Consumo de crudo refinería Shushufindi" | 
            #`...1` == "Consumo de crudo planta destiladora Lago Agrio" | 
-           `...1` == "COMPAÑÍAS PRIVADAS-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
+           #`...1` == "COMPAÑÍAS PRIVADAS-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
            #`...1` == "- Producción Promedio Diaria-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)" | 
            `...1` == "TOTAL NACIONAL-EXPORTACIONES DE PETRÓLEO CRUDO (Miles de barriles)" | 
-           `...1` == "EMPRESAS PÚBLICAS-EXPORTACIONES DE PETRÓLEO CRUDO (Miles de barriles)" | 
+           #`...1` == "EMPRESAS PÚBLICAS-EXPORTACIONES DE PETRÓLEO CRUDO (Miles de barriles)" | 
            #`...1` == "Crudo Oriente (11)" | 
            #`...1` == "Crudo Napo (12)" | 
-           `...1` == "CÍAS. PRIVADAS**" | 
-           `...1` == "MINISTERIO DE ENERGÍA Y MINAS (MEM)" | 
+           #`...1` == "CÍAS. PRIVADAS**" | 
+           #`...1` == "MINISTERIO DE ENERGÍA Y MINAS (MEM)" | 
            `...1` == "TOTAL NACIONAL-CRUDO RECIBIDO EN REFINERÍAS Y CONSUMO EN OLEODUCTO (Miles de barriles)" | 
            #`...1` == "- Promedio Diario Recibido" | 
            #`...1` == "Refinería Esmeraldas" | 
@@ -153,6 +156,8 @@ data_1 = data_inicial %>%
            `...1` == "Precio Promedio Mensual WTI" | 
            `...1` == "Precio Promedio Mensual Brent"
            )
+} # Nombre de las variables
+
 data_2 %>% 
   count(Indicador, sort = TRUE)
 
@@ -160,6 +165,7 @@ colnames(data_1) = c("Indicador", "2007_01", "2007_02", "2007_03", "2007_04", "2
 
 data_2 = select(data_1, "Indicador", "2007_01", "2007_02", "2007_03", "2007_04", "2007_05", "2007_06", "2007_07", "2007_08", "2007_09", "2007_10", "2007_11", "2007_12", "2008_01", "2008_02", "2008_03", "2008_04", "2008_05", "2008_06", "2008_07", "2008_08", "2008_09", "2008_10", "2008_11", "2008_12", "2009_01", "2009_02", "2009_03", "2009_04", "2009_05", "2009_06", "2009_07", "2009_08", "2009_09", "2009_10", "2009_11", "2009_12", "2010_01", "2010_02", "2010_03", "2010_04", "2010_05", "2010_06", "2010_07", "2010_08", "2010_09", "2010_10", "2010_11", "2010_12", "2011_01", "2011_02", "2011_03", "2011_04", "2011_05", "2011_06", "2011_07", "2011_08", "2011_09", "2011_10", "2011_11", "2011_12", "2012_01", "2012_02", "2012_03", "2012_04", "2012_05", "2012_06", "2012_07", "2012_08", "2012_09", "2012_10", "2012_11", "2012_12", "2013_01", "2013_02", "2013_03", "2013_04", "2013_05", "2013_06", "2013_07", "2013_08", "2013_09", "2013_10", "2013_11", "2013_12", "2014_01", "2014_02", "2014_03", "2014_04", "2014_05", "2014_06", "2014_07", "2014_08", "2014_09", "2014_10", "2014_11", "2014_12", "2015_01", "2015_02", "2015_03", "2015_04", "2015_05", "2015_06", "2015_07", "2015_08", "2015_09", "2015_10", "2015_11", "2015_12", "2016_01", "2016_02", "2016_03", "2016_04", "2016_05", "2016_06", "2016_07", "2016_08", "2016_09", "2016_10", "2016_11", "2016_12", "2017_01", "2017_02", "2017_03", "2017_04", "2017_05", "2017_06", "2017_07", "2017_08", "2017_09", "2017_10", "2017_11", "2017_12", "2018_01", "2018_02", "2018_03", "2018_04", "2018_05", "2018_06", "2018_07", "2018_08", "2018_09", "2018_10", "2018_11", "2018_12", "2019_01", "2019_02", "2019_03", "2019_04", "2019_05", "2019_06", "2019_07", "2019_08", "2019_09", "2019_10", "2019_11", "2019_12", "2020_01", "2020_02", "2020_03", "2020_04", "2020_05", "2020_06", "2020_07", "2020_08", "2020_09", "2020_10", "2020_11", "2020_12", "2021_01", "2021_02", "2021_03", "2021_04", "2021_05", "2021_06", "2021_07", "2021_08", "2021_09", "2021_10", "2021_11", "2021_12", "2022_01", "2022_02", "2022_03", "2022_04", "2022_05", "2022_06", "2022_07", "2022_08", "2022_09", "2022_10", "2022_11", "2022_12", "2023_01", "2023_02", "2023_03", "2023_04", "2023_05", "2023_06", "2023_07", "2023_08", "2023_09", "2023_10", "2023_11", "2023_12", "2024_01", "2024_02", "2024_03", "2024_04", "2024_05", "2024_06")
 
+{
 data_3 = data_2 %>% 
   pivot_longer(
     cols = c("2007_01", "2007_02", "2007_03", "2007_04", "2007_05", "2007_06", 
@@ -200,6 +206,7 @@ data_3 = data_2 %>%
     names_to = "date", 
     values_to = "value"
   )
+} # Formato de fechas
 
 data_4 = data_3 %>%
   pivot_wider(id_cols = c("date"), names_from = "Indicador", values_from = c("value"))
@@ -211,47 +218,39 @@ data_5 = data_4 %>%
   select(fecha,everything()) %>%
   select(-date)
 
-data_6$fecha <- as.Date(data_6$fecha, format = "%Y-%m-%d")
-
-
 # Cambiar formato valores
-
-data_6$fecha <- as.Date(data_6$fecha, format = "%Y-%m-%d")
-
 data_6= data_5%>%
   mutate(across(where(is.character), as.numeric))
 
 data_6$fecha <- as.Date(data_6$fecha, format = "%Y-%m-%d")
 
 # Casos NA
-
+{
 data_7 = data_6 %>% 
   replace_na(list(n = 0))
 
 data_8 = data_7 %>% 
   mutate_all(~replace_na(., 0))
+}
+sum(is.na(data_6))
+colSums(is.na(data_6))
 
-any(is.na(data_8))
-sum(is.na(data_8))
-str(data_8)
-colSums(is.na(data_8))
-
-# Tendencia Central -------------------------------------------------------
+  # Estadística descriptiva -------------------------------------------------------
 # Opción de análisis: "describe" y "summary"
 # Producción de Petroleo Crudo-TOTAL NACIONAL
-describe(data_8$`TOTAL NACIONAL-Producción de Petróleo Crudo`)
+describe(data_6$`TOTAL NACIONAL-Producción de Petróleo Crudo`)
 
 # Porducción de petróleo crudo fiscalizada (miles de barriles)- TOTAL NACIONAL
-describe(data_8$`TOTAL NACIONAL-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)`)
+describe(data_6$`TOTAL NACIONAL-PRODUCCIÓN DE PETRÓLEO CRUDO FISCALIZADA (Miles de barriles)`)
 
 #Exportaciones de petróleo cudo (miles de barriles)-TOTAL NACIONAL
-describe(data_8$`TOTAL NACIONAL-EXPORTACIONES DE PETRÓLEO CRUDO (Miles de barriles)`)
+describe(data_6`TOTAL NACIONAL-EXPORTACIONES DE PETRÓLEO CRUDO (Miles de barriles)`)
 
 # Importaciones de derivados-TOTAL NACIONAL
-describe(data_8$`TOTAL NACIONAL-IMPORTACIÓN DE DERIVADOS (Miles de barriles)`)
+describe(data_6$`TOTAL NACIONAL-IMPORTACIÓN DE DERIVADOS (Miles de barriles)`)
 
 # Ingresos de EP PETROECUADOR por exportaciones (miles de USD)
-describe(data_8$`Ingresos de EP PETROECUADOR por Exportaciones (miles de USD)`)
+describe(data_6$`Ingresos de EP PETROECUADOR por Exportaciones (miles de USD)`)
 
 # Gráfica -----------------------------------------------------------------
 ggplot(data_6, aes(x = fecha)) +
---
+++ sector_financiero/sector_financiero/sector_financiero.R
@@ -0,0 +1,260 @@
+{
+library(readxl)
+library(tidyverse)
+library(lubridate)
+library(zoo)
+library(psych)
+library(ggplot2)
+library(stats)
+library(modeest)
+}
+
+
+# Limpieza ----------------------------------------------------------------
+data <- read_excel("C:/Users/USER/Documents/YRF_PROJECT/sector_financiero/data/modificada/BMS_CAMBIOS.xlsx")
+
+{
+colnames(data) = c("año", "mes", "dia","reservas_internacionales", "pasivos_monetarios", "emision_monetaria",
+                   "dinero_electronico", "reservas_bancarias", "depositos_a_la_vista", "cuasidinero_total",
+                   "cuasidinero_ahorro", "cuasidinero_plazo", "cuasidinero_restringido", "cuasidinero_operaciones_de_reporto",
+                   "cuasidinero_otros_depositos", "credito_al_sector_privado_total", "credito_al_sector_privado_cartera",
+                   "crédito_al_sector_privado_otros", "tasas_de_interes_referenciales_porcentajes_basica",
+                   "tasas_de_interes_referenciales_porcentajes_pasiva", "tasas_de_interes_referenciales_porcentajes_activa",
+                   "tasas_de_interes_referenciales_porcentajes_activa_de_corto_plazo_para_el", "inflacion_mensual", "inflacion_anual",
+                   "inflacion_acumulada")
+
+}# Nombres de las variables
+
+{
+# Reservas internacionales  RI(11) // reservas_internacionales
+# Pasivos Monetarios  PM (1) // pasivos_monetarios
+# Emisión monetaria EM(2) // emision_monetaria
+# Dinero Electrónico DE(3) // dinero_electronico
+# Reservas bancarias (Rb) // reservas_bancarias
+# Depósitos a la vista (4) // depositos_a_la_vista
+# Cuasidinero Total // cuasidinero_total
+# Cuasidinero Ahorro // cuasidinero_ahorro
+# Cuasidinero Plazo // cuasidinero_plazo
+# Cuasidinero Restringido // cuasidinero_restringido
+# Cuasidinero Operaciones de reporto // cuasidinero_operaciones_de_reporto
+# Cuasidinero Otros depósitos // cuasidinero_otros_depositos
+# Crédito al sector privado(6) Total // credito_al_sector_privado_total
+# Crédito al sector privado(6) Cartera // credito_al_sector_privado_cartera
+# Crédito al sector privado(6) Otros // crédito_al_sector_privado_otros
+# TASAS DE INTERÉS REFERENCIALES porcentajes Básica (7) // tasas_de_interes_referenciales_porcentajes_basica
+# TASAS DE INTERÉS REFERENCIALES porcentajes Pasiva (8) // tasas_de_interes_referenciales_porcentajes_pasiva
+# TASAS DE INTERÉS REFERENCIALES porcentajes Activa (9) // tasas_de_interes_referenciales_porcentajes_activa
+# TASAS DE INTERÉS REFERENCIALES porcentajes Activa de Corto Plazo para el segmento Productivo Corporativo (10) // tasas_de_interes_referenciales_porcentajes_activa_de_corto_plazo_para_el segmto_productivo_corporativo
+# INFLACIÓN porcentajes Mensual // inflacion_mensual
+# INFLACIÓN porcentajes Anual // inflacion_anual
+# INFLACIÓN porcentajes Acumulada // inflacion_acumulada
+}# Definir nombres
+
+{
+data_clean <- data %>% select(1:25) %>% filter(!if_all(everything(), is.na)) # seleccionar todas la variables
+
+data_clean$año = na.locf(data_clean$año) # mezclar todas las variables para formato fecha
+
+# Mapeo de nombres de meses en español a números
+month_map <- setNames(1:12, c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",                              
+                              "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"))
+
+# Transformar `mes` de nombres en español a numéricos y combinar `año`, `mes`, `dia` en una `fecha`
+
+data_clean2 <- data_clean %>%
+  mutate(mes = month_map[mes],
+         date = make_date(año, mes, dia)) %>%
+  select(-año, -mes, -dia) %>%
+  rename(fecha = date) %>%
+  relocate(fecha, .before = everything())  # Mueve la columna fecha al primer lugar
+
+}# Fechas
+
+{
+  sapply(data_clean2, class)
+  
+  data_clean3 <- data_clean2 %>%
+    mutate(across(where(is.character) & !contains("fecha"), as.numeric))
+  
+  sapply(data_clean3, class)
+}# Convertir variables a numericas
+
+# Limpieza de datos atipicos
+
+# Valores "n.d" y "n.d." a NA
+colSums(is.na(data_clean3))
+
+data_clean3[] <- lapply(data_clean3, function(x) {
+  if (is.character(x)) x[x %in% c("n.d", "n.d.")] <- NA
+  return(x)
+})
+
+# Eliminar valores NA
+
+
+# Estadística descriptiva ---------------------------------------------------------
+# Reservas internacionales
+summary(data_clean3$reservas_internacionales)
+describe(data_clean3$reservas_internacionales)
+var(data_clean3$reservas_internacionales)
+
+#Pasivos monenetarios
+summary(data_clean3$pasivos_monetarios)
+describe(data_clean3$pasivos_monetarios)
+var(data_clean3$pasivos_monetarios)
+
+# Emisión monetaria
+summary(data_clean3$emision_monetaria)
+describe(data_clean3$emision_monetaria)
+var(data_clean3$emision_monetaria)
+
+# Reservas bancarias
+summary(data_clean3$reservas_bancarias)
+describe(data_clean3$reservas_bancarias)
+var(data_clean3$reservas_bancarias)
+
+# Depositos a la vista
+summary(data_clean3$depositos_a_la_vista)
+describe(data_clean3$depositos_a_la_vista)
+var(data_clean3$depositos_a_la_vista, na.rm = TRUE)
+
+# Cuasidinero total
+summary(data_clean3$cuasidinero_total)
+describe(data_clean3$cuasidinero_total)
+var(data_clean3$cuasidinero_total, na.rm = TRUE)
+
+# Credito al   sector privado total
+summary(data_clean3$credito_al_sector_privado_total)
+describe(data_clean3$credito_al_sector_privado_total)
+var(data_clean3$credito_al_sector_privado_total, na.rm = TRUE)
+
+# Tasas de interés referecniales (%) básicas
+summary(data_clean3$tasas_de_interes_referenciales_porcentajes_basica)
+describe(data_clean3$tasas_de_interes_referenciales_porcentajes_basica)
+var(data_clean3$tasas_de_interes_referenciales_porcentajes_basica, na.rm = TRUE)
+
+# Tasas de interés referecniales (%) pásiva
+summary(data_clean3$tasas_de_interes_referenciales_porcentajes_pasiva)
+describe(data_clean3$tasas_de_interes_referenciales_porcentajes_pasiva)
+var(data_clean3$tasas_de_interes_referenciales_porcentajes_pasiva, na.rm = TRUE)
+
+# Tasas de interés referecniales (%) activa
+summary(data_clean3$tasas_de_interes_referenciales_porcentajes_activa)
+describe(data_clean3$tasas_de_interes_referenciales_porcentajes_activa)
+var(data_clean3$tasas_de_interes_referenciales_porcentajes_activa, na.rm = TRUE)
+
+# Inflación mensual
+summary(data_clean3$inflacion_mensual)
+describe(data_clean3$inflacion_mensual)
+var(data_clean3$inflacion_mensual, na.rm = TRUE)
+
+# Inflación anual
+summary(data_clean3$inflacion_anual)
+describe(data_clean3$inflacion_anual)
+var(data_clean3$inflacion_anual, na.rm = TRUE)
+
+# Inflación acumulada
+summary(data_clean3$inflacion_acumulada)
+describe(data_clean3$inflacion_acumulada)
+var(data_clean3$inflacion_acumulada, na.rm = TRUE)
+
+
+colSums(is.na(data_clean3))
+
+
+# Gráfica -----------------------------------------------------------------
+# Reservas internacionales
+ggplot(data_clean3, aes(x = fecha, y = reservas_internacionales))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+#Pasivos monenetarios
+ggplot(data_clean3, aes(x = fecha, y = pasivos_monetarios))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Emisión monetaria
+ggplot(data_clean3, aes(x = fecha, y = emision_monetaria))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Reservas bancarias
+ggplot(data_clean3, aes(x = fecha, y = reservas_bancarias))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Depositos a la vista
+ggplot(data_clean3, aes(x = fecha, y = depositos_a_la_vista))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Cuasidinero total
+ggplot(data_clean3, aes(x = fecha, y = cuasidinero_total))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Credito al   sector privado total
+ggplot(data_clean3, aes(x = fecha, y = credito_al_sector_privado_total))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Tasas de interés referecniales (%) básicas
+ggplot(data_clean3, aes(x = fecha, y = tasas_de_interes_referenciales_porcentajes_basica))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Tasas de interés referecniales (%) activa
+ggplot(data_clean3, aes(x = fecha, y = tasas_de_interes_referenciales_porcentajes_activa))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Tasas de interés referecniales (%) pásiva
+ggplot(data_clean3, aes(x = fecha, y = tasas_de_interes_referenciales_porcentajes_pasiva))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Inflación mensual
+ggplot(data_clean3, aes(x = fecha, y = inflacion_mensual))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+# Inflación anual
+ggplot(data_clean3, aes(x = fecha, y = inflacion_anual))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+
+# Inflación acumulada
+ggplot(data_clean3, aes(x = fecha, y = inflacion_acumulada))+
+  geom_line()+
+  geom_point()+
+  theme_minimal() +
+  theme_classic()
+
+
+
+
